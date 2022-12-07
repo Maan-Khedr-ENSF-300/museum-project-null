a6ca2dcb0768582541ab06d988e1a5a45e0d151c
mizybermas <mizy.bermas@ucalgary.ca>
2022-12-07 16:14:58 -0700

    prompting for username and password and validating before main menu

---
+++ sqlScripts/main.py
@@ -0,0 +1,14 @@
+from validate import *
+from getpass import getpass
+
+def main():
+
+    user = input("Username: ")
+    password = getpass("Password: ")
+    valid_credentials = validateCredentials(user, password)
+    while not valid_credentials:
+        user = input("Username: ")
+        password = getpass("Password: ")
+        valid_credentials = validateCredentials(user, password)
+
+    main_menu = input("\n1.\tManage Art Objects\n2.\tUser Management")
