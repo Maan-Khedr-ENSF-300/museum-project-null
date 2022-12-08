f1f7f1686b12a2ed6367e532d6723df8c2e884fd
Joshua Jipp <joshuajipp@gmail.com>
2022-12-07 17:51:39 -0700

    added function to validate database name

--- pythonProgram/main.py
+++ pythonProgram/main.py
@@ -1,23 +1,47 @@
 from validate import *
 from getpass import getpass
 
+
 def main():
+    print("\n1.\tLogin as guest\n2.\tLogin as admin")
+    user_type = input("Enter a number to choose option: ")
 
-    user = input("Username: ")
-    password = getpass("Password: ")
-    valid_credentials = validateCredentials(user, password)
-    while not valid_credentials:
-        user = input("Username: ")
-        password = getpass("Password: ")
-        valid_credentials = validateCredentials(user, password)
+    if user_type == '1':
+        pass
 
-    main_menu = input("\n1.\tManage Art Objects\n2.\tUser Management")
-    valid_option = validateMenuOption(main_menu)
-    while not valid_option:
-        print("Invalid input. Please select from the following options:")
-        main_menu = input("\n1.\tManage Art Objects\n2.\tUser Management")
-        valid_option = validateMenuOption(main_menu)
+    elif user_type == '2':
+        valid_credentials = False
+        while not valid_credentials:
+            user = input("Username: ")
+            password = getpass("Password: ")
+            valid_credentials = validateCredentials(user, password)
+        validate_schema = False
+        while not validate_schema:
+            user_schema = input("Enter the name of the database to use: ")
+            validate_schema = validateSchema(user, password, user_schema)
+    else:
+        print(f'Selection "{user_type}" is invalid.')
 
-    if main_menu == "1":
-        sub_option =  input("\na.\tAdd art pieces to the database\nb.\tSearch art pieces and exhibitions")
-        valid_sub_option = validateSubOption(sub_option)
+    # user = input("Username: ")
+    # password = getpass("Password: ")
+    # valid_credentials = validateCredentials(user, password)
+    # while not valid_credentials:
+    #     user = input("Username: ")
+    #     password = getpass("Password: ")
+    #     valid_credentials = validateCredentials(user, password)
+
+    # main_menu = input("\n1.\tManage Art Objects\n2.\tUser Management")
+    # valid_option = validateMenuOption(main_menu)
+    # while not valid_option:
+    #     print("Invalid input. Please select from the following options:")
+    #     main_menu = input("\n1.\tManage Art Objects\n2.\tUser Management")
+    #     valid_option = validateMenuOption(main_menu)
+
+    # if main_menu == "1":
+    #     sub_option = input(
+    #         "\na.\tAdd art pieces to the database\nb.\tSearch art pieces and exhibitions")
+    #     valid_sub_option = validateSubOption(sub_option)
+
+
+if __name__ == '__main__':
+    main()
--- pythonProgram/validate.py
+++ pythonProgram/validate.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 def validateCredentials(username, pword) -> bool:
     try:
         mysql.connector.connect(
@@ -12,14 +13,30 @@ def validateCredentials(username, pword) -> bool:
             raise error
     return True
 
+
+def validateSchema(username, pword, db):
+    try:
+        mysql.connector.connect(
+            host="127.0.0.1", port=3306, user=username, password=pword, database=db)
+
+    except Exception as error:
+        if f"Unknown database '{db}'" in str(error):
+            print(f'Database "{db}" does not exist')
+            return False
+        else:
+            raise error
+    return True
+
+
 def validateMenuOption(main_menu):
-    options = ["1","2"]
+    options = ["1", "2"]
     if main_menu in options:
         return True
     return False
 
+
 def validateSubOption(sub_option):
-    options = ["a","b"]
+    options = ["a", "b"]
     if sub_option.lower() in options:
         return True
     return False
