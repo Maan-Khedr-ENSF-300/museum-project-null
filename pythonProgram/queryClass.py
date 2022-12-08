fda03d6d5a3a9c1791d97a739518700391e86b49
Joshua Jipp <joshuajipp@gmail.com>
2022-12-07 20:59:05 -0700

    added helper functions and query method

--- pythonProgram/queryClass.py
+++ pythonProgram/queryClass.py
@@ -16,3 +16,30 @@ class QueryBuilder():
         tmp = self.data.cursor(buffered=True)
         tmp.execute(query)
         self.data.commit()
+
+    def query(self, table, columns, condition=None, order_by=None, return_query_flag=False):
+        tmp = self.data.cursor(buffered=True)
+        select_str = f"SELECT {columns} FROM {table}"
+        if condition != None:
+            select_str += f" WHERE {condition}"
+        if order_by != None:
+            select_str += f" ORDER BY {order_by}"
+
+        tmp.execute(select_str)
+        if return_query_flag:
+            return select_str
+        else:
+            select_str += ';'
+            return tmp.fetchall()
+
+    def columnInfo(self, table):
+        tmp = self.data.cursor(buffered=True)
+        tmp.execute(f"SELECT * FROM {table}")
+        column_info = [[i[0], i[1], i[6]] for i in tmp.description]
+
+        return column_info
+
+    def getTableNames(self):
+        tmp = self.data.cursor(buffered=True)
+        tmp.execute("SHOW TABLES")
+        return [x[0] for x in tmp]
